cdd3948c31c90a363debe6a5266409972442c507
Your Name <you@example.com>
2019-02-28 15:17:17 +0100

    Login

--- src/tasks/login/gui/components/layout/login/mainarea.js
+++ src/tasks/login/gui/components/layout/login/mainarea.js
@@ -37,6 +37,9 @@ const MainAreaStyle = {
         padding: 20,
         justifyContent: 'center',
         textAlign: 'center'
+    },
+    image : {
+        verticalAlign : 'middle'
     }
 }
 
@@ -103,8 +106,8 @@ class MainArea extends React.Component {
                     <div style={MainAreaStyle.loginInputs}>
                         <br />
                         <img src="/login/static/ax5.png" width="75" height="75" draggable="false"/><br /><br /><br />
-                        <img src="/login/static/user.png" width="15" height="15" draggable="false"/> <input key="user" style={mainStyle.inputStyle} type="text" value={this.state.user} onChange={this.userChanged} onBlur={this.userChanged}/> <br /><br />
-                        <img src="/login/static/password.png" width="15" height="15" draggable="false"/> <input key="password" style={mainStyle.inputStyle} type="password" value={this.state.password} onChange={this.passwordChanged} onBlur={this.passwordChanged} onKeyPress={this.keyPressed}/>
+                        <span><img style={MainAreaStyle.image} src="/login/static/user.png" width="15" height="15" draggable="false"/> <input key="user" style={mainStyle.inputStyle} type="text" value={this.state.user} onChange={this.userChanged} onBlur={this.userChanged}/></span> <br /><br />
+                        <span><img style={MainAreaStyle.image} src="/login/static/password.png" width="15" height="15" draggable="false"/> <input key="password" style={mainStyle.inputStyle} type="password" value={this.state.password} onChange={this.passwordChanged} onBlur={this.passwordChanged} onKeyPress={this.keyPressed}/></span>
                     </div>
                 </div>
             </div>
